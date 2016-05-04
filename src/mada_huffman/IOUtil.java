1407b929dcd513f261d3b8cf04052237a2381bda
= <patric.steiner@students.fhnw.ch>
2016-05-04 16:14:28 +0200

    bitstring to byte array and vice versa works now

--- src/mada_huffman/IOUtil.java
+++ src/mada_huffman/IOUtil.java
@@ -31,4 +31,44 @@ public class IOUtil {
     	fos.write(bytes);
     	fos.close();
     }
+    
+    static byte[] binaryStringToByteArray(String binaryString) throws Exception {
+        assert binaryString.matches("^[01]*$");
+        binaryString += "1";
+        //make sure string length is a multiple of 8 by appending 10* at the end
+        //every string must end with 1 and then x*0 (empty string becomes = 10000000)
+        if (binaryString.length() % 8 != 0) {
+            for (int i = 8 - binaryString.length() % 8; i > 0; i--)
+                binaryString += "0";
+        }
+        byte[] bytes = new byte[binaryString.length()/8];
+        String substring;
+        byte b;
+        for (int i = 0; i < bytes.length; i++) {
+            //get next 8 chars
+            substring = binaryString.substring(8*i, 8*i + 8);
+            b = 0;
+            for (int j = 0; j < 8; j++) {
+                b |= (substring.charAt(j) == '0') ? 0 : 1;
+                if (j < 7 ) b <<= 1; //else there is 1 shift too much
+            }
+            bytes[i] = b;
+        }
+        return bytes;
+        
+    }
+    
+    static String byteArrayToBinaryString(byte[] bytes) {
+        assert bytes.length > 0;
+        assert bytes[bytes.length - 1] != 0;
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            for(int i = 0; i < 8; i++) {
+                sb.append((b & 0b10000000) == 0b10000000 ? "1" : "0");
+                b <<= 1;
+            }
+        }
+        sb.delete(sb.lastIndexOf("1"), sb.length());
+        return sb.toString();
+    }
 }
---
+++ test/mada_huffman/IOUtilTest.java
@@ -0,0 +1,49 @@
+package mada_huffman;
+import static org.junit.Assert.*;
+import static mada_huffman.IOUtil.*;
+
+import org.junit.Test;
+
+public class IOUtilTest {
+
+	@Test
+	public void testByteArrayToBinaryString() {
+	    try {
+	        byteArrayToBinaryString(new byte[] {});
+            fail();
+        }
+        catch (AssertionError a) {  }
+        try {
+            byteArrayToBinaryString(new byte[] {0});
+            fail();
+        }
+        catch (AssertionError a) {  }
+        
+		assertEquals("0000000", byteArrayToBinaryString(new byte[] {1}));
+		assertEquals("000000", byteArrayToBinaryString(new byte[] {2}));
+		assertEquals("000000010000000", byteArrayToBinaryString(new byte[] {1, 1}));
+		assertEquals("0000010001111111000001", byteArrayToBinaryString(new byte[] {4, 127 ,6}));
+	}
+	
+	@Test
+	public void testBinaryStringToByteArray() throws Exception {
+		try {
+			binaryStringToByteArray("01");
+			fail();
+		}
+		catch (AssertionError a) {  }
+		try {
+			binaryStringToByteArray("0101010a");
+			fail();
+		}
+		catch (AssertionError a) {  }
+		
+		assertArrayEquals(new byte[] {(byte) 0b10000000 }, binaryStringToByteArray(""));
+		assertArrayEquals(new byte[] {(byte) 0b11110010 }, binaryStringToByteArray("111100"));
+		assertArrayEquals(new byte[] {(byte) 0b11110000, (byte) 0b11110001}, binaryStringToByteArray("111100001111000"));
+		assertArrayEquals(new byte[] {(byte) 0b11111111, (byte) 0b10000001, (byte) 0b10101010, (byte) 0b10000000 }, binaryStringToByteArray("111111111000000110101010"));
+		
+		assertEquals("1010101", byteArrayToBinaryString(binaryStringToByteArray("1010101")));
+	}
+
+}
